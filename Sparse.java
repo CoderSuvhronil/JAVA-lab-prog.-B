43445369c906f12e8f064d977cd02964a670600f
Suvhronil Ghosal <146948850+CoderSuvhronil@users.noreply.github.com>
2025-01-21 21:27:01 +0530

    Update Sparse.java

--- Sparse.java
+++ Sparse.java
@@ -1,51 +1,51 @@
 import java.util.Arrays;
 
-class Sparse {
-    // Represent a sparse matrix in Triplet format
-    public void toTripletFormat(int[][] sparseMatrix) {
-        int m = sparseMatrix.length;
-        int n = sparseMatrix[0].length;
-        
+public class Sparse {
+    public void toTripletFormat(int sp_matrix[][]) {
+        int m = sp_matrix.length;
+        int n = sp_matrix[0].length;
+
         int nonZeroCount = 0;
+
+        // Count non-zero elements
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (sparseMatrix[i][j] != 0) {
+                if (sp_matrix[i][j] != 0) {
                     nonZeroCount++;
                 }
             }
         }
-        
-        int[][] triplet = new int[nonZeroCount + 1][3];
+
+        // Create triplet array
+        int triplet[][] = new int[nonZeroCount + 1][3];
         triplet[0][0] = m;
         triplet[0][1] = n;
         triplet[0][2] = nonZeroCount;
-        
+
         int k = 1;
+
+        // Fill the triplet array
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (sparseMatrix[i][j] != 0) {
+                if (sp_matrix[i][j] != 0) {
                     triplet[k][0] = i;
                     triplet[k][1] = j;
-                    triplet[k][2] = sparseMatrix[i][j];
+                    triplet[k][2] = sp_matrix[i][j];
                     k++;
                 }
             }
         }
-        
-        // Display the triplet format
+
+        // Print the triplet representation
         System.out.println("Triplet Representation:");
         for (int i = 0; i < triplet.length; i++) {
             System.out.println(Arrays.toString(triplet[i]));
         }
     }
 
-    public static void main(String[] args) {
-        int[][] sparseMatrix = {
-            {0, 0, 3},
-            {4, 0, 0},
-            {0, 2, 0}
-        };
-        Sparse sparse = new Sparse();
-        sparse.toTripletFormat(sparseMatrix);
+    public static void main(String args[]) {
+        int[][] sp_matrix = {{0, 0, 3}, {4, 0, 0}, {0, 2, 0}};
+        Sparse sp = new Sparse();
+        sp.toTripletFormat(sp_matrix);
     }
 }
